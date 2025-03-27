460b708054b82093af354a6151ae01634ba6671d
Lukeguy334 <lhs57@usf.edu>
2025-03-27 14:58:17 -0400

    Debug

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-import { getUsers } from './api.js';
+import { getPosts } from './api.js';
 
-function displayUsers() {
-    const container = document.getElementById('userContainer');
+function displayPosts() {
+    const container = document.getElementById('postContainer');
 
-    getUsers()
-        .then(users => {
-            users.forEach(user => {
+    getPosts()
+        .then(posts => {
+            posts.slice(0, 5).forEach(post => {
                 const div = document.createElement('div');
-                div.innerHTML = `<h3>${user.name}</h3><p>${user.email}</p>`;
+                div.innerHTML = `<h3>${post.name}</h3><p>${post.email}</p>`;
                 container.appendChild(div);
             });
         })
         .catch(err => {
-            container.innerHTML = `<p style="color:red;">Failed to load users: ${err.message}</p>`;
+            container.innerHTML = `<p style="color:red;">Failed to load posts: ${err.message}</p>`;
         });
 }
 
-displayUsers();
+displayPosts();
