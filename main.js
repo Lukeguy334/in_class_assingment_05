0e3a45dbc834a5c22e503214975109eae1062c03
Lukeguy334 <lhs57@usf.edu>
2025-03-27 14:46:02 -0400

    Updated code

---
+++ main.js
@@ -0,0 +1,19 @@
+import { getUsers } from './api.js';
+
+function displayUsers() {
+    const container = document.getElementById('userContainer');
+
+    getUsers()
+        .then(users => {
+            users.forEach(user => {
+                const div = document.createElement('div');
+                div.innerHTML = `<h3>${user.name}</h3><p>${user.email}</p>`;
+                container.appendChild(div);
+            });
+        })
+        .catch(err => {
+            container.innerHTML = `<p style="color:red;">Failed to load users: ${err.message}</p>`;
+        });
+}
+
+displayUsers();
